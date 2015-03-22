6655ace0b12a04702881fb753de8a902b4722db5
USER <USER@192.168.25.236>
2015-03-21 22:04:03 -0400

    cadastro de alunos Primeira parte

---
+++ CadastroAluno/src/main/java/cadastro/Aluno.java
@@ -0,0 +1,45 @@
+package cadastro;
+
+public class Aluno {
+	private String nome, rg, cpf, sexo, estado;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+}
